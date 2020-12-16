c04255f43191064b7c91b2f7525129c16af06bf1
Daniel Lopes <daniel.lopes.santoss@gmail.com>
2020-12-16 18:26:56 -0300

    Criação da Árvore sintática

--- lfa.py
+++ lfa.py
@@ -1,6 +1,6 @@
 class No:
     # A classe que vai representar cada nó de nossa árvore
-    def __init__(self, tipo, operador, id=None, filho_esq=None, filho_dir=None ):
+    def __init__(self, tipo, operador, id=None, filho_esq=None, filho_dir=None):
         # Construtor da Classe Nó
 
         # Argumentos:
@@ -18,29 +18,51 @@ class No:
         self.nullable = False  # Verdadeiro se conseguimos chegar na palavra vazia por esse nó
         self.firstpos = set()  # firstpos of node (refer to documentation.md for detail).
         self.lastpos = set()  # followpos of node (refer to documentation.md for detail).
-class arvore:
+
+    def __repr__(self):
+        '''In console string'''
+        childrenCount = int(self.filho_dir != None) + int(self.filho_esq != None)
+        s = "<" + "'{0}'".format(self.tipo) + ' Node with label ' + "'{0}'".format(self.operador) + ' and ' + str(
+            childrenCount) + [' child', ' children'][childrenCount != 1] + '>'
+        return s
+class Arvore:
     # Essa classe controi a árvore sintática, que é muito importante para a conversão para AFD
     # Ela recebe a expressão regular como entrada
     def __init__(self, exRe):
-        self.raiz = self.criar_arvore(exRe)
         self.folha = dict()
         self.id_contador = 1
+        self.raiz = self.criar_arvore(exRe)
 
-
+        print(self.raiz)
+        print(type(self.raiz))
+        #print(self.raiz)
+        #print(self.raiz)
         #self.followpos = [set() for i in range(self.id_contador)]
         #self.postorder_nullable_firstpos_lastpos_followpos(self.raiz)
     def criar_arvore(self, exRe):
+        print(exRe)
         if (exRe != []):
             token = exRe[0]
             if token == ".":
-                
+                del exRe[0]
+                filho_esq, n_exRe = self.criar_arvore(exRe)
+                filho_dir, nn_exRe = self.criar_arvore(n_exRe)
+                id  = self.contador_id()
+                return No("conc", token, id, filho_esq, filho_dir), nn_exRe
             elif token == "+":
-                
+                del exRe[0]
+                filho_esq, n_exRe = self.criar_arvore(exRe)
+                filho_dir, nn_exRe = self.criar_arvore(n_exRe)
+                id = self.contador_id()
+                return No("ou", token, id, filho_esq, filho_dir), nn_exRe
             elif token == "*":
-                
+                del exRe[0]
+                filho_esq, n_exRe = self.criar_arvore(exRe)
+                id  = self.contador_id()
+                return No("fecho", token, id, filho_esq), n_exRe
             else: #letra
                 del exRe[0]
-                return No("letra", token,id  = self.contador_id()), exRe
+                return No("letra", token, id  = self.contador_id()), exRe
         else:
             return None
     def contador_id(self):
@@ -92,6 +114,7 @@ def main():
     # Passo 1, processa o input
     ALFABETO, ExRe = ler_input()
     print(ALFABETO, ExRe)
+    Arvore(ExRe)
 
 if __name__ == "__main__":
     main()
