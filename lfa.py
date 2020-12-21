ba72132d3f96316cb1c7cca86da7d827ebc31ea6
Daniel Lopes <daniel.lopes.santoss@gmail.com>
2020-12-20 22:17:40 -0300

    Comentando o código

--- lfa.py
+++ lfa.py
@@ -1,6 +1,6 @@
 import sys
 class Estado:
-    # Construtor da classe estado
+    # Construtor da classe Estado
     # Args:
     #  Alfabeto da expressão regular
     #  Lista de folhas
@@ -21,7 +21,7 @@ class AFD:
         self.final = arvore.id_contador-1
         self.computa_estados(arvore)
     def computa_estados(self, arvore):
-        D1 = Estado(self.alfabeto, arvore.raiz.firstpos, self.proximo_id(), self.final)
+        D1 = Estado(self.alfabeto, arvore.raiz.primeiraPos, self.proximo_id(), self.final)
         self.estados.append(D1)
         queue = [D1]
         while len(queue) > 0:  # Encontra as transicoes pros estados
@@ -43,9 +43,9 @@ class AFD:
                 continue
             label = arvore.folhas[i]
             if estado.transicoes[label] == {}:
-                estado.transicoes[label] = arvore.followpos[i]
+                estado.transicoes[label] = arvore.proximaPos[i]
             else:
-                estado.transicoes[label] = estado.transicoes[label].union(arvore.followpos[i])
+                estado.transicoes[label] = estado.transicoes[label].union(arvore.proximaPos[i])
         for a in self.alfabeto:
             if estado.transicoes[a] != {}:
                 new = True
@@ -107,109 +107,111 @@ class No:
         self.tipo = tipo
         self.filho_esq = filho_esq
         self.filho_dir = filho_dir
+       
+        self.nulo = False  # Verdadeiro se conseguimos chegar na palavra vazia por esse nó
         
-        self.nullable = False  # Verdadeiro se conseguimos chegar na palavra vazia por esse nó
-        self.firstpos = set()  # firstpos of node (refer to documentation.md for detail).
-        self.lastpos = set()  # followpos of node (refer to documentation.md for detail).
+        # Abaixo são meta dados da árvore que nos ajudaram a fazer a relação entre a árvore e os autômatos 
+        self.primeiraPos = set()  
+        self.ultimaPos = set()  
 
 class Arvore:
     # Essa classe controi a árvore sintática, que é muito importante para a conversão para AFD
     # Ela recebe a expressão regular como entrada
     def __init__(self, exRe):
-        self.folhas = dict()
-        self.id_contador = 1
-        raiz_aux, lixo= self.criar_arvore(exRe)
+        self.folhas = dict()    # Cria um atributo para armezar mais fácilmente as folhas
+        self.id_contador = 1    # Contador de nós que são letras
+        raiz_aux, lixo= self.criar_arvore(exRe) # Função de criar a árvore. A expressão regular que ela retorna deve ser vazia, ou teve algum erro
         if(lixo != []): # Não deveria acontecer
             print("Erro") 
-        no_aux = No('letra', '#', id=self.contador_id())
-        self.raiz = No("conc", ".", None, raiz_aux, no_aux)
+        no_aux = No('letra', '#', id=self.contador_id())    # A nossa árvore sempre vai ter o primeiro nó como uma concatenação, o filho a esquerda
+        self.raiz = No("conc", ".", None, raiz_aux, no_aux) # é a árvore normal, e o filho a direita é um nó do tipo letra com o operador "#"
 
-        self.followpos = [set() for i in range(self.id_contador)]
-        self.nullable_firstpos_lastpos_followpos(self.raiz)
-    def criar_arvore(self, exRe):
-        if (exRe != []):
+        self.proximaPos = [set() for i in range(self.id_contador)] 
+        self.nulo_primeiraPos_ultimaPos_proximaPos(self.raiz) # Após a árvore estar pronta, chamamos essa função para anotar os meta dados dela
+    def criar_arvore(self, exRe): #Função recursiva de criar a árvore
+        if (exRe != []): # Enquanto a expressão regular não for vazia, continuamos
             token = exRe[0]
-            if token == ".":
-                del exRe[0]
+            if token == ".":    # Se for uma concatenação, fazemos recursivamente o filho da esqueda e depois o da direita e deletamos o primeiro 
+                del exRe[0]     # elemento da Expressão Regular, pois acabamos de analisar ele
                 filho_esq, n_exRe = self.criar_arvore(exRe)
                 filho_dir, nn_exRe = self.criar_arvore(n_exRe)
                 return No("conc", token, None, filho_esq, filho_dir), nn_exRe
-            elif token == "+":
+            elif token == "+":  # Mesma lógica da concatenação
                 del exRe[0]
                 filho_esq, n_exRe = self.criar_arvore(exRe)
                 filho_dir, nn_exRe = self.criar_arvore(n_exRe)
                 return No("ou", token, None, filho_esq, filho_dir), nn_exRe
-            elif token == "*":
+            elif token == "*":  # No fecho, chamamos a função apenas para o filho da esquerda
                 del exRe[0]
                 filho_esq, n_exRe = self.criar_arvore(exRe)
                 return No("fecho", token, None, filho_esq), n_exRe
-            else: #letra
+            else: #letra    # Caso base. Aqui não chamamos a função criar_arvore() novamente
                 del exRe[0]
-                if(len(token) == 1):
+                if(len(token) == 1): # Se a string que estamos analisando tiver apenas uma letra, criamos um nó e acabam as chamadas recursivas por enquanto
                     id  = self.contador_id()
                     self.folhas[id] = token
                     return No("letra", token, id), exRe
-                else:
-                    return self.string_multipla(token), exRe # Para o caso da string ter mais que uma letra
+                else:   # Se a string que estamos analisando tiver duas ou mais letras, como "aa", "bba", "aaabc", por exemplo, transformaremos ela
+                    return self.string_multipla(token), exRe # numa sequencia concatenada, transformando "aa" em "a.a", "bba" em "b.b.a" e assim em diante
         else:
             return None
-    def string_multipla(self, palavra): # Funcção para o caso da string ter mais que uma letra. Isto é, o elemente da lista é "aa", e então temos que
-        if(len(palavra) == 1):          # transforma em a concatenado com a
+    def string_multipla(self, palavra): # Função para o caso da string ter mais que uma letra. Isto é, se o elemente da lista é "aa", então temos que
+        if(len(palavra) == 1):          # transforma em a concatenado com a. Esta linha é o caso base.
             id = self.contador_id()
             self.folhas[id] = palavra
             return No("letra", palavra, id)
-        else:
-            id = self.contador_id()
-            self.folhas[id] = palavra[0]
+        else:                           # Se a palavra em questão ainda for maior que uma letra apenas, criamos um nó de concatenação, com o filho a 
+            id = self.contador_id()     # esquerda como letra atual(ex, se a palavra for "aaa", a letra atual é apenas "a"), e mandamos o resto("aa") para
+            self.folhas[id] = palavra[0]# a recursão como o filho direito do nó
             filho_esq = No("letra", palavra[0], id)
             filho_dir = self.string_multipla(palavra[1:])
             return No("conc", ".", None, filho_esq, filho_dir)
-    def contador_id(self):
+    def contador_id(self):              # Apenas para ser um contador
         id = self.id_contador
         self.id_contador += 1
         return id
-    def nullable_firstpos_lastpos_followpos(self, no):
+    def nulo_primeiraPos_ultimaPos_proximaPos(self, no):
         if not no:
             return
-        # esq
-        self.nullable_firstpos_lastpos_followpos(no.filho_esq)
-        # dir
-        self.nullable_firstpos_lastpos_followpos(no.filho_dir)
+        # Segue recursivamente a esquerda
+        self.nulo_primeiraPos_ultimaPos_proximaPos(no.filho_esq)
+        # Segue recursivamente a direita
+        self.nulo_primeiraPos_ultimaPos_proximaPos(no.filho_dir)
         #raiz
-        if no.tipo == "letra":
-            if no.operador == "@":  # empty char
-                no.nullable = True
+        if no.tipo == "letra":  # Se é uma folha, primeira e ultima posição serão o id desses nós
+            if no.operador == "@":  # palavra vazia
+                no.nulo = True
             else:
-                no.firstpos.add(no.id)
-                no.lastpos.add(no.id)
-        elif no.tipo == "ou":
-            no.nullable = no.filho_esq.nullable or no.filho_dir.nullable
-            no.firstpos = no.filho_esq.firstpos.union(no.filho_dir.firstpos)
-            no.lastpos = no.filho_esq.lastpos.union(no.filho_dir.lastpos)
-        elif no.tipo == "fecho":
-            no.nullable = True
-            no.firstpos = no.filho_esq.firstpos
-            no.lastpos = no.filho_esq.lastpos
-            self.compute_follows(no) 
-        elif no.tipo == "conc":
-            no.nullable = no.filho_esq.nullable and no.filho_dir.nullable
-            if no.filho_esq.nullable:
-                no.firstpos = no.filho_esq.firstpos.union(no.filho_dir.firstpos)
+                no.primeiraPos.add(no.id)
+                no.ultimaPos.add(no.id)
+        elif no.tipo == "ou":   # Se é concatenação, será nulo se algum dos filhos for nulo, a primeira e a ultima posição será a união da primeira 
+            no.nulo = no.filho_esq.nulo or no.filho_dir.nulo # e da ultima pos dos filhos
+            no.primeiraPos = no.filho_esq.primeiraPos.union(no.filho_dir.primeiraPos)
+            no.ultimaPos = no.filho_esq.ultimaPos.union(no.filho_dir.ultimaPos)
+        elif no.tipo == "fecho": # Pode ser nulo, e como só tem filho a esquerda, primeira e ultima pos será igual ao dele
+            no.nulo = True
+            no.primeiraPos = no.filho_esq.primeiraPos
+            no.ultimaPos = no.filho_esq.ultimaPos
+            self.computa_proximaPos(no) # Chamamos a função de computar prox posição
+        elif no.tipo == "conc": # Se é concatenação, o nó nulo tem que ter os dois filhos podendo ser nulos
+            no.nulo = no.filho_esq.nulo and no.filho_dir.nulo
+            if no.filho_esq.nulo: # Se o filho esq é nulo, a primeira pos é a união da primeira pos dos dois filhos
+                no.primeiraPos = no.filho_esq.primeiraPos.union(no.filho_dir.primeiraPos)
+            else: # Senão, é apenas a do filho esq 
+                no.primeiraPos = no.filho_esq.primeiraPos
+            if no.filho_dir.nulo: # Similar ao caso acima, mas para o filho direito e para a ultima pos
+                no.ultimaPos = no.filho_esq.ultimaPos.union(no.filho_dir.ultimaPos)
             else:
-                no.firstpos = no.filho_esq.firstpos
-            if no.filho_dir.nullable:
-                no.lastpos = no.filho_esq.lastpos.union(no.filho_dir.lastpos)
-            else:
-                no.lastpos = no.filho_dir.lastpos
-            self.compute_follows(no)
+                no.ultimaPos = no.filho_dir.ultimaPos
+            self.computa_proximaPos(no) 
         return
-    def compute_follows(self, no):
+    def computa_proximaPos(self, no): # Apenas no fecho e na concatenação chamamos essa função. Ela apenas computa o atributo de proxPos do nó
         if no.tipo == "conc":
-            for i in no.filho_esq.lastpos:
-                self.followpos[i] = self.followpos[i].union(no.filho_dir.firstpos)
+            for i in no.filho_esq.ultimaPos:
+                self.proximaPos[i] = self.proximaPos[i].union(no.filho_dir.primeiraPos)
         elif no.tipo == "fecho":
-            for i in no.filho_esq.lastpos:
-                self.followpos[i] = self.followpos[i].union(no.filho_esq.firstpos)
+            for i in no.filho_esq.ultimaPos:
+                self.proximaPos[i] = self.proximaPos[i].union(no.filho_esq.primeiraPos)
 def op(caractere):
     # Diz se o char é uma letra ou um operador
     if caractere == "." or caractere =="*" or caractere =="+":
